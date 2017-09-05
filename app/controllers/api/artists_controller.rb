3085e91eafe455fbec5c12a7c941dba217466c17
cmsegui <cmsegui@yahoo.com>
2017-09-05 15:48:48 -0400

    did a bunch of shit idk what

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,49 +1,54 @@
 class Api::ArtistsController < ApplicationController
-    def index
-      @artists = Artist.all
-      render json: @artists
-    end
-  
-    def create
-      @artist = Artist.new(artist_params)
-      
-      if @artist.save
-        render json: @artist
-      else
-        render json: {
-            message: 'Error when creating Artist'
-        }
-      end
-    end
-  
-    def show
-      @artist = Artist.find(params[:id])
+  def index
+    @artists = Artist.all
+    render json: @artists
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @songs = @artist.songs.all
+    render json: {
+      artist: @artist,
+      songs: @songs
+    }
+  end
+
+  def create
+    @artist = Artist.new artist_params
+
+    if @artist.save
       render json: @artist
-    end
-  
-    def update
-      @artist = Artist.find params[:id]
-      
-      if @artist.update!(artist_params)
-       render json: @artist
-      else 
-        render json: {
-            message: 'Artist successfully updated!'
-        }
-      end
-    end
-  
-    def destroy
-      @artist = Artist.find(params[:id])
-      @artist.destroy
-    
+    else
       render json: {
-          message: 'Artist successfully destroyed'
+        message: 'Error when creating Artist'
       }
     end
-  
-    private
-    def artist_params
-      params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
+
+  def update
+    @artist = Artist.find params[:id]
+
+    if @artist.update(artist_params)
+      render json: @artist
+    else
+      render json: {
+        message: 'Error when updating Artist'
+      }
     end
   end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+
+    render json: {
+      message: 'Artist successfully destroyed'
+    }
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
+end
